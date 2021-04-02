5e455b07a27c9e23ebc6f0ea24a7782fa4be62d9
Alexandre Xavier <jalexandre.xavier@gmail.com>
2021-04-02 06:48:49 +0100

    change comments

--- main.go
+++ main.go
@@ -3,29 +3,29 @@ package main
 import "github.com/fogleman/ln/ln"
 
 func main() {
-	// create a scene and add a single cube
+	// criar a sena
 	scene := ln.Scene{}
 	scene.Add(ln.NewCube(ln.Vector{-1, -1, -1}, ln.Vector{1, 1, 1}))
 
-	// define camera parameters
-	eye := ln.Vector{4, 3, 2}    // camera position
-	center := ln.Vector{0, 0, 0} // camera looks at
-	up := ln.Vector{0, 0, 1}     // up direction
+	// defenir os parametros da camera
+	eye := ln.Vector{4, 3, 2}    // posisão camera
+	center := ln.Vector{0, 0, 0} // objectivo da camera
+	up := ln.Vector{0, 0, 1}     // direcão
 
-	// define rendering parameters
-	width := 1024.0  // rendered width
-	height := 1024.0 // rendered height
-	fovy := 50.0     // vertical field of view, degrees
+	// defenir os parametros do render
+	width := 1024.0  // width
+	height := 1024.0 // height
+	fovy := 50.0     // degrees
 	znear := 0.1     // near z plane
 	zfar := 10.0     // far z plane
-	step := 0.01     // how finely to chop the paths for visibility testing
+	step := 0.01
 
-	// compute 2D paths that depict the 3D scene
+	// caminhos 2D
 	paths := scene.Render(eye, center, up, width, height, fovy, znear, zfar, step)
 
-	// render the paths in an image
+	// render para uma imagem
 	paths.WriteToPNG("out.png", width, height)
 
-	// save the paths as an svg
+	// gravar o caminho em SVG
 	paths.WriteToSVG("out.svg", width, height)
 }
